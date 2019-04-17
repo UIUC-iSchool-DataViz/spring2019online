d14d27d018e349e98a22da16971a3f25038e2a91
Jill P Naiman <jnaiman@gmail.com>
2019-04-16 19:01:08 -0500

    map of world

---
+++ week13/corg_website_example/components/.#map-custom-d3-component.js
@@ -0,0 +1,1 @@
+jillnaiman1@Jills-MacBook-Pro-2.local.14139
--- week13/corg_website_example/components/map-custom-d3-component.js
+++ week13/corg_website_example/components/map-custom-d3-component.js
@@ -13,6 +13,7 @@ const d3tip = require('d3-tip');
 var margin = {top: 0, right: 0, bottom: 0, left: 0},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
+var format = d3.format(",");
 
 
 class MapCustomD3Component extends D3Component {
@@ -25,8 +26,6 @@ class MapCustomD3Component extends D3Component {
             .attr('class', 'map');
 
 
-	var format = d3.format(",");
-
 	// Set tooltips
 	var tip = d3tip() // JPN: had to change here d3.tip -> d3tip
             .attr('class', 'd3-tip')
@@ -37,8 +36,26 @@ class MapCustomD3Component extends D3Component {
 	
 	
 	var color = d3.scaleThreshold()
-	    .domain([10000,100000,500000,1000000,5000000,10000000,50000000,100000000,500000000,1500000000])
-	    .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)","rgb(33,113,181)","rgb(8,81,156)","rgb(8,48,107)","rgb(3,19,43)"]);
+	    .domain([10000,
+		     100000,
+		     500000,
+		     1000000,
+		     5000000,
+		     10000000,
+		     50000000,
+		     100000000,
+		     500000000,
+		     1500000000])
+	    .range(["rgb(247,251,255)",
+		    "rgb(222,235,247)",
+		    "rgb(198,219,239)",
+		    "rgb(158,202,225)",
+		    "rgb(107,174,214)",
+		    "rgb(66,146,198)",
+		    "rgb(33,113,181)",
+		    "rgb(8,81,156)",
+		    "rgb(8,48,107)",
+		    "rgb(3,19,43)"]);
 
 
 	var path = d3.geoPath();
@@ -56,13 +73,16 @@ class MapCustomD3Component extends D3Component {
 
 	d3.queue() // JPN: had to change queue -> d3.queue()
 	    .defer(d3.json, "https://raw.githubusercontent.com/jdamiani27/Data-Visualization-and-D3/master/lesson4/world_countries.json")
-	    .defer(d3.tsv, "world_population.tsv")
+	    .defer(d3.tsv, "https://raw.githubusercontent.com/jnaiman/champaignElection/master/website_test/random_data/world_population.tsv")
 	    .await(ready);
 
 	function ready(error, data, population) {
+	    if (error) throw error;
 	    var populationById = {};
 	    
 	    population.forEach(function(d) { populationById[d.id] = +d.population; });
+	    //console.log('YO');
+	    //console.log(population);
 	    data.features.forEach(function(d) { d.population = populationById[d.id] });
 	    
 	    svg.append("g")
@@ -79,7 +99,7 @@ class MapCustomD3Component extends D3Component {
 		.style("stroke","white")
 		.style('stroke-width', 0.3)
 		.on('mouseover',function(d){
-		    tip.show(d);
+		    tip.show(d, this);
 		    
 		    d3.select(this)
 			.style("opacity", 1)
@@ -97,7 +117,8 @@ class MapCustomD3Component extends D3Component {
 	    
 	    svg.append("path")
 		.datum(topojson.mesh(data.features, function(a, b) { return a.id !== b.id; }))
-	    // .datum(topojson.mesh(data.features, function(a, b) { return a !== b; }))
+	    //.datum(topojson.mesh(data.features, function(a, b) { return a !== b; }))
+		//.attr("d", topojson.mesh(data.features, function(a, b) { return a.id !== b.id; }) )
 		.attr("class", "names")
 		.attr("d", path);
 	}
